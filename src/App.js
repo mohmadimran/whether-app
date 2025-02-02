aa95596f4841608c9cab8e77a51c359c6c727266
mohmadimran <pathanimrankhan308@gmail.com>
2025-02-02 20:25:20 +0530

    fix the issue of loading state

--- src/App.js
+++ src/App.js
@@ -12,26 +12,25 @@ function App() {
     if (!city) return;
 
     setLoading(true);
-    axios.get(
-      `https://api.weatherapi.com/v1/current.json?key=4d819f9a077d475da1282000250202&q=${city}`
-    )
+    axios
+      .get(
+        `https://api.weatherapi.com/v1/current.json?key=4d819f9a077d475da1282000250202&q=${city}`
+      )
       .then((res) => {
-        setLoading(false) ;
         setData(res.data);
       })
       .catch((err) => {
-        setLoading(false) ;
         console.error(err);
-        alert("Failed to fetch weather data")
+        alert("Failed to fetch weather data");
       })
-
+      .finally(() => setLoading(false));
   }, [city]);
 
   return (
     <div className="whether-page-container">
       <SearchBar searchCity={searchCity} />
-        {loading && <p>Loading data…</p>}
-        {data && <Card weatherData={data} />}
+      {loading && (<p>Loading data…</p>)}
+      {data && <Card weatherData={data} />}
     </div>
   );
 }
